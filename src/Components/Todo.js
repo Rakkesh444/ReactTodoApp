a6be08fb3157b1cd015e27f690beeb778abd3d9c
Rakkesh444 <rakkeshit@gmail.com>
2024-09-22 13:07:22 +0530

    React Todo App

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -6,7 +6,7 @@ export const Todo = ({ task, taskCompleted, Deleted, editTodo }) => {
         <div className="flex justify-between bg-purple-500 p-3 rounded-lg mb-2">
             <p onClick={() => taskCompleted(task.id)} className={task.completed ? "Completed" : ""}><span className="text-white">{task.task}</span></p>
             <div className="flex gap-12 relative">
-                <FontAwesomeIcon icon={faEdit} onClick={() => editTodo(task.id)} className="cursor-pointer text-emerald-200 text-xl hover:text-white" />
+                <FontAwesomeIcon icon={faEdit} onClick={() => editTodo(task.id)} className="cursor-pointer text-red-800 text-xl hover:text-white" />
                 <FontAwesomeIcon icon={faTrash} onClick={() => Deleted(task.id)} className="cursor-pointer text-red-500 text-xl hover:text-red-700 transition duration-100" />
             </div>
         </div >
